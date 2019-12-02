a20ff9b9f970c2304f500cc439d93c30d11295f4
Piggy-gry <18883748118@163.com>
2019-12-03 00:26:49 +0800

    创建训练表单中添加容器相关参数

--- src/common/js/global-constant.js
+++ src/common/js/global-constant.js
@@ -30,8 +30,18 @@ const trainStatus = [
   }
 ];
 
+const memory = ['4G', '8G', '16G'];
+const cpuSpeci = ['intel i3', 'intel i5', 'intel i7'];
+const cpuCore = ['1核', '2核', '4核'];
+const gpuMemory = ['2G', '4G', '8G', '16G'];
+const gpuNum = ['1个', '2个', '4个'];
 export default {
   algoTypes,
   framework,
-  trainStatus
+  trainStatus,
+  memory,
+  cpuCore,
+  cpuSpeci,
+  gpuMemory,
+  gpuNum
 };
