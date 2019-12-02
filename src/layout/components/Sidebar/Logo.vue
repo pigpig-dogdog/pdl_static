8eda6c5ee658505ee3059af7dc678fab6fdbb264
Piggy-gry <18883748118@163.com>
2019-12-03 02:59:43 +0800

    1.使logo点击回到datasets首页 2.数据集封面上传不做图片大小控制

--- src/layout/components/Sidebar/Logo.vue
+++ src/layout/components/Sidebar/Logo.vue
@@ -1,14 +1,12 @@
 <template>
   <div class="sidebar-logo-container" :class="{'collapse':collapse}">
     <transition name="sidebarLogoFade">
-      <router-link v-if="collapse" key="collapse" class="sidebar-logo-link" to="/">
+      <router-link v-if="collapse" key="collapse" class="sidebar-logo-link" to="/datasets/index">
         <svg-icon v-if="logo" icon-class="logo" class="sidebar-logo"/>
-        <!-- <img v-if="logo" src="@/assets/logo.jpeg" class="sidebar-logo"> -->
         <h1 v-else class="sidebar-title">{{ title }} </h1>
       </router-link>
-      <router-link v-else key="expand" class="sidebar-logo-link" to="/">
+      <router-link v-else key="expand" class="sidebar-logo-link" to="/datasets/index">
         <svg-icon v-if="logo" icon-class="logo" class="sidebar-logo"/>
-        <!-- <img v-if="logo" src="@/assets/logo.jpeg" class="sidebar-logo"> -->
         <h1 class="sidebar-title">{{ title }} </h1>
       </router-link>
     </transition>
--- src/views/datasets/detail-components/DetailHeader.vue
+++ src/views/datasets/detail-components/DetailHeader.vue
@@ -147,14 +147,10 @@ export default {
     beforeCoverUpload (file) {
       const isJPG = file.type === 'image/jpeg';
       const isPNG = file.type === 'image/png';
-      const isLt2M = file.size / 1024 / 1024 < 2;
       if (!isJPG && !isPNG) {
         this.$message.error('上传头像图片只能是 JPG 格式或 PNG 格式!');
       }
-      if (!isLt2M) {
-        this.$message.error('上传头像图片大小不能超过 2MB!');
-      }
-      return (isJPG || isPNG) && isLt2M;
+      return isJPG || isPNG;
     }
   }
 };
